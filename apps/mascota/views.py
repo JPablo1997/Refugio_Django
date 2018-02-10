8c9718bfcb3a0e0a3add8a1d4e4f9df33fb6a7e0
Pablo_97 <dc16009@ues.edu.sv>
2018-02-10 11:33:48 -0600

    Update mediante una view basada en funciones

--- apps/mascota/views.py
+++ apps/mascota/views.py
@@ -20,6 +20,17 @@ def mascota_view(request):
 	return render(request, 'mascota/mascota_form.html', {'form':form})
 
 def mascota_list(request):
-	mascota = Mascota.objects.all()
+	mascota = Mascota.objects.all().order_by('id')
 	contexto = {'mascotas':mascota}
 	return render(request, 'mascota/mascota_list.html', contexto)
+
+def mascota_edit(request, id_mascota):
+	mascota = Mascota.objects.get(id=id_mascota)
+	if request.method == 'GET':
+		form = MascotaForm(instance=mascota)
+	else:
+		form = MascotaForm(request.POST, instance=mascota)
+		if form.is_valid():
+			form.save()
+		return redirect('http://127.0.0.1:8000/mascota/listar/')
+	return render(request, 'mascota/mascota_form.html', {'form':form})
