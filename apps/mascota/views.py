89ead65f649125bdadc67ce6f85817f63979d847
Pablo_97 <dc16009@ues.edu.sv>
2018-02-10 09:35:52 -0600

    Inicializando el repositorio

---
+++ apps/mascota/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from apps.mascota.forms import MascotaForm
+
+
+# Create your views here.
+def index(request):
+	return render(request, 'mascota/index.html')
+
+def mascota_view(request):
+	if request.method == 'POST':
+		form = MascotaForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('http://127.0.0.1:8000/mascota/')
+
+	else:
+		form = MascotaForm()
+
+	return render(request, 'mascota/mascota_form.html', {'form':form})
