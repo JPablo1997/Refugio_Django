0f95bf12c0614d2078b1759691ad9438ea68b5bf
Pablo_97 <dc16009@ues.edu.sv>
2018-02-11 18:28:19 -0600

    CRUD con dos formularios: Persona y Solicitud

--- apps/adopcion/urls.py
+++ apps/adopcion/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from apps.adopcion.views import index_adopcion
+from apps.adopcion.views import index_adopcion, SolicitudList, SolicitudCreate
 
 urlpatterns = [
     path('index/', index_adopcion),
+    path('solicitud/listar/', SolicitudList.as_view(), name="solicitud_listar"),
+    path('solicitud/nueva/', SolicitudCreate.as_view(), name="solicitud_crear"),
 ]
