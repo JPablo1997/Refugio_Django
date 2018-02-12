da4848a93ccbe5bc56782ec4fbbb9b18ccd35496
Pablo_97 <dc16009@ues.edu.sv>
2018-02-12 11:26:59 -0600

    DELETE - CRUD con dos formularios

--- apps/adopcion/urls.py
+++ apps/adopcion/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from apps.adopcion.views import index_adopcion, SolicitudList, SolicitudCreate
+from apps.adopcion.views import index_adopcion, SolicitudList, SolicitudCreate, SolicitudUpdate
 
 urlpatterns = [
     path('index/', index_adopcion),
     path('solicitud/listar/', SolicitudList.as_view(), name="solicitud_listar"),
     path('solicitud/nueva/', SolicitudCreate.as_view(), name="solicitud_crear"),
+    path('solicitud/editar/<int:pk>/', SolicitudUpdate.as_view(), name = "solicitud_editar")
 ]
