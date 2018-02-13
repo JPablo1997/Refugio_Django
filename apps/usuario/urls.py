78044ae3249cf9ecccb642afbee6da821eda8a28
Pablo_97 <dc16009@ues.edu.sv>
2018-02-13 13:50:23 -0600

    Restframework ModelSerializer

--- apps/usuario/urls.py
+++ apps/usuario/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from apps.usuario.views import RegistroUsuario
+from apps.usuario.views import RegistroUsuario, UserAPI
 
 urlpatterns = [
     path('registrar/', RegistroUsuario.as_view(), name='registrar'),
+    path('api/', UserAPI.as_view(), name='api'),
 ]
